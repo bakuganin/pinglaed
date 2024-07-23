96f7b4af4a0a5969d6db689e6ce8710b385ec3d9
PetrTolstov <talkingthetruth7461@gmail.com>
2024-07-23 16:19:19 +0300

    update

--- js/calcForm.js
+++ js/calcForm.js
@@ -1,22 +1,58 @@
-document.getElementById('CalculatorForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвращает стандартное поведение формы
+document.getElementById('CalculatorForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Предотвращает стандартное поведение формы
 
-    var form = event.target;
-    var formData = new FormData(form);
+  var form = event.target;
+  var formData = new FormData(form);
 
-    fetch(form.action, {
-        method: 'POST',
-        body: formData,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(function(response) {
-        if (response.ok) {
-            window.location.href = 'https://www.youtube.com/'; // Замените на ваш URL
-        } else {
-            console.log('Ошибка отправки формы');
-        }
-    }).catch(function(error) {
-        console.log('Ошибка:', error);
+  // Создаем объект, чтобы хранить имена полей и их значения
+  var data = {};
+  formData.forEach((value, key) => {
+    if (!data[key]) {
+      data[key] = value;
+    } else {
+      if (!Array.isArray(data[key])) {
+        data[key] = [data[key]];
+      }
+      data[key].push(value);
+    }
+  });
+
+  // Выводим имена полей и их значения
+  for (const key in data) {
+    if (Array.isArray(data[key])) {
+      data[key].forEach((val, index) => {
+        console.log(`${key}_${index + 1}: ${val}`);
+      });
+    } else {
+      console.log(`${key}: ${data[key]}`);
+    }
+  };
+
+  fetch(form.action, {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(function (response) {
+    document.getElementById("CalculatorForm").style.display = "none"
+    if (response.ok) {
+      document.getElementById("success-form").style.display = "block"
+    } else {
+      document.getElementById("error-form").style.display = "block"
+      console.log('Ошибка отправки формы');
+    }
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
+  }).catch(function (error) {
+    document.getElementById("error-form").style.display = "block"
+    console.log('Ошибка отправки формы');
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    console.log('Ошибка:', error);
+  });
 });
---
+++ js/phoneForm.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('BackCall-Form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвращает стандартное поведение формы
+
+    var form = event.target;
+    var formData = new FormData(form);
+
+    // Создаем объект, чтобы хранить имена полей и их значения
+    var data = {};
+    formData.forEach((value, key) => {
+      if (!data[key]) {
+        data[key] = value;
+      } else {
+        if (!Array.isArray(data[key])) {
+          data[key] = [data[key]];
+        }
+        data[key].push(value);
+      }
+    });
+
+    // Выводим имена полей и их значения
+    for (const key in data) {
+      if (Array.isArray(data[key])) {
+        data[key].forEach((val, index) => {
+          console.log(`${key}_${index + 1}: ${val}`);
+        });
+      } else {
+        console.log(`${key}: ${data[key]}`);
+      }
+    };
+
+    fetch(form.action, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(function (response) {
+      document.getElementById("BackCall-Form").style.display = "none"
+      if (response.ok) {
+        document.getElementById("success-form-2").style.display = "block"
+      } else {
+        document.getElementById("error-form-2").style.display = "block"
+        console.log('Ошибка отправки формы');
+      }
+    }).catch(function (error) {
+      document.getElementById("error-form-2").style.display = "block"
+      console.log('Ошибка отправки формы');
+      console.log('Ошибка:', error);
+    });
+  });
+
+
+  document.getElementById('ZamerCall-Form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Предотвращает стандартное поведение формы
+
+    var form = event.target;
+    var formData = new FormData(form);
+
+    // Создаем объект, чтобы хранить имена полей и их значения
+    var data = {};
+    formData.forEach((value, key) => {
+      if (!data[key]) {
+        data[key] = value;
+      } else {
+        if (!Array.isArray(data[key])) {
+          data[key] = [data[key]];
+        }
+        data[key].push(value);
+      }
+    });
+
+    // Выводим имена полей и их значения
+    for (const key in data) {
+      if (Array.isArray(data[key])) {
+        data[key].forEach((val, index) => {
+          console.log(`${key}_${index + 1}: ${val}`);
+        });
+      } else {
+        console.log(`${key}: ${data[key]}`);
+      }
+    };
+
+    fetch(form.action, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(function (response) {
+      if (response.ok) {
+        document.getElementById("success-form-3").style.display = "block"
+      } else {
+        document.getElementById("error-form-3").style.display = "block"
+        console.log('Ошибка отправки формы');
+      }
+    }).catch(function (error) {
+      document.getElementById("error-form-3").style.display = "block"
+      console.log('Ошибка отправки формы');
+      console.log('Ошибка:', error);
+    });
+  });
+
+
+});
